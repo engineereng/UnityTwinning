ba1f7fcb2de5e82e6d12844a2ec56875e666e87e
colline <collineng@gmail.com>
2023-06-22 13:42:50 -0700

    add managers

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Singleton<SingletonClass> : MonoBehaviour where SingletonClass : MonoBehaviour {
+
+	public static SingletonClass Instance;
+
+	protected virtual void Awake() {
+		if (Instance is not null) {
+			Destroy(gameObject);
+			return;
+		}
+
+		Instance = this as SingletonClass;
+	}
+
+
+	private void OnApplicationQuit() {
+		Instance = null;
+		Destroy(gameObject);
+	}
+
+}
