21037009b67a89876c4b70c8e5c3a0b863a449ec
Collin Eng <eng.engineereng@gmail.com>
2023-06-24 15:52:53 -0700

    wip calibration of feedback

--- Server/index.js
+++ Server/index.js
@@ -15,43 +15,76 @@ let board = new five.Board();
 
 var angle = 0;
 var servoPin = 9;
+var analogPin = "A0";
 var clientAngle = 0;
 var isClientInCharge = true;
+var isSweeping = false;
+
+// https://learn.adafruit.com/analog-feedback-servos/using-feedback
+// Calibration values
+var minDegrees;
+var maxDegrees;
+var minFeedback;
+var maxFeedback;
+var tolerance = 2; // max feedback measurement error
+
+// implemnts Arduino's map()
+// https://stackoverflow.com/questions/70643627/python-equivalent-for-arduinos-map-function
+function map_range(x, in_min, in_max, out_min, out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 board.on("ready", () => {
-    const positionPin = new five.Pin("A0");
+    const positionPin = new five.Pin(analogPin);
     const servo = new five.Servo({pin: servoPin, startAt: angle});
 
+    // calibrate(servo, "A0", 0, 180);
+
+      // Move to the minimum position and record the feedback value
+    minDegrees = 0;
+    servo.to(90);
+    board.wait(2000, () =>
+    {
+      positionPin.query((state) => {
+        console.log("state: " + state.value)
+        minFeedback = state.value;
+    
+      // Move to the maximum position and record the feedback value
+        servo.to(179);
+        maxDegrees = 179;
+        board.wait(4000, () => 
+          positionPin.query((state1) => {         console.log("state1: " + state1.value);          maxFeedback = state1.value;}));
+      })
+    });
+    
     positionPin.read(function(error, value) {
       // console.log("New servo angle: " + value);
       board.wait(100, () => {
-        angle = value;
+        var interpolatedValue = map_range(value, minFeedback, maxFeedback, 0, 180);
+        angle = interpolatedValue;
       });
     })
     board.loop(5000, () => {
-      if (!isClientInCharge)
+      if (!isClientInCharge && !isSweeping)
       {
+        isSweeping = true;
         console.log("Beginning sweep.");
         servo.sweep();      
         board.wait(3000, () => {
           console.log("Stopping sweep.");
           servo.stop();
+          isSweeping = false;
           isClientInCharge = true;
         });
       }
     });
 
     board.loop(100, () => {
-      // if (!isSweeping)
-      // {
-        // console.log("Moving to clientAngle: " + clientAngle);
-        if (isClientInCharge)
-        {
-          waitingForClientInput = false;
-          servo.to(clientAngle);
-          board.wait(100, () => {isClientInCharge = false;}) 
-        }
-      // }
+      if (isClientInCharge)
+      {
+        servo.to(clientAngle);
+      }
     })
     board.repl.inject({
       servo: servo
@@ -63,7 +96,7 @@ wss.on('connection', function(ws) {
 
   // send angle interval
   const textInterval = setInterval(() => {
-    if (!isClientInCharge)
+    if (!isClientInCharge && isSweeping)
       ws.send(angle);
       console.log("Sending angle to client: " + angle);
   }, 100);
@@ -89,7 +122,6 @@ wss.on('connection', function(ws) {
   ws.on('close', function() {
     console.log("client left.");
     clearInterval(textInterval);
-    clearInterval(binaryInterval);
   });
 });
 
